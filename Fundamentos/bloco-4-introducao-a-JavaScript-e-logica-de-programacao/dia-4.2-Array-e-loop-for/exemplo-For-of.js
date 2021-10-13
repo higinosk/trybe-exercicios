a6cdcd8b72a9271a2e7e7a880082d6fbc69a5d7c
higinosk <kamilly.higinos@gmail.com>
2021-10-13 15:32:38 -0300

    Adicionado exemplo-For-of.js

---
+++ Fundamentos/bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-4.2-Array-e-loop-for/exemplo-For-of.js
@@ -0,0 +1,15 @@
+//For of
+//Objetos iteráveis: são objetos ou uma estrutura de dados que permite acessar o seu conteúdo com um for.
+
+
+// let arrOfNumbers = [10, 20, 30];
+// for (let sum of arrOfNumbers) {
+//   sum += 10;
+//   console.log(sum);
+// }
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let sum of names) {
+    sum += 12;
+    console.log(sum)
+}
