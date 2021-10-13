31289676cfb522f110a896e5f6be6515a7db0fe0
higinosk <kamilly.higinos@gmail.com>
2021-10-13 15:20:26 -0300

    Adicionado exemplo-For.js

---
+++ Fundamentos/bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-4.2-Array-e-loop-for/exemplo-For.js
@@ -0,0 +1,12 @@
+//For
+// var numero = 7
+// for(var contador = 0; contador <= 10; contador++) {
+//     console.log('Tabuada do 7')
+//     console.log(numero * contador);
+// }
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index =0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
