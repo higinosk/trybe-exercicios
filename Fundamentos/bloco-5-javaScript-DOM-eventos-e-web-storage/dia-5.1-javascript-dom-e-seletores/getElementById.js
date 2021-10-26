9f651c6dd7fafdaeb8941e239caca6575fee83e4
higinosk <kamilly.higinos@gmail.com>
2021-10-26 18:39:06 -0300

    praticas

---
+++ Fundamentos/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-5.1-javascript-dom-e-seletores/getElementById.js
@@ -0,0 +1,19 @@
+
+document.getElementById("page-title").innerText = "Contratiempo"
+
+// const paragraph = document.getElementById("paragraph");
+//       paragraph.style.color = "red";
+      document.getElementById("subtitle").innerText = "O que eu acho?"
+// document.getElementById("second-paragraph").innerText = "Melhor filme!"
+
+document.getElementsByClassName("paragraph")[0].style.color = "red";
+document.getElementsByTagName("h4")[0].style.color = "blue";
+//console.log(document.getElementById("test").innerText);
+
+
+//#document guarda toda a estrutura da pagina html, dá acesso ao DOM.
+//.innerHTMl retorna o texto puro.
+//innerText retorna o conteúdo de fato.
+//Os dois permitem alterar o conteúdo. ex. document.getElementById("test").innerText = "aprendizados da aula de hoje";
+//getElementById recebe um parametro da id
+
